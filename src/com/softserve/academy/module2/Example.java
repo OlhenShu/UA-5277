7fc127090f40123cfd04078e53c89e4d0072ade5
Olhen Shu <olya.shutileva@gmail.com>
2026-07-27 14:01:18 +0300

    add additional output message to Example.java

--- src/com/softserve/academy/module2/Example.java
+++ src/com/softserve/academy/module2/Example.java
@@ -3,5 +3,6 @@ package com.softserve.academy.module2;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello from module 2!");
+        System.out.println("This is an example of a Java class in module 2.");
     }
 }
