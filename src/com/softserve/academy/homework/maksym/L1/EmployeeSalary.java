9eb0652a885bad37cf9fe77ae68e7587d738629f
Maksym.Y <maksym.yeremenko@gmail.com>
2026-08-05 01:03:12 +0300

    Duplicating previous work (P1.2)

---
+++ src/com/softserve/academy/homework/maksym/L1/EmployeeSalary.java
@@ -0,0 +1,16 @@
+package com.softserve.academy.homework.maksym.L1;
+
+public class EmployeeSalary {
+    public static void main(String[] args ) {
+        String employeeName = "Ivan";
+        double monthlySalary = 1500.0;
+        int months = 12;
+        double bonus = 500.0;
+        double yearlySalary = (monthlySalary * months) + bonus;
+
+        System.out.println("Employee: "+ employeeName);
+        System.out.println("Monthly salary: "+ monthlySalary + " UAH.");
+        System.out.println("Bonus: " + bonus + " UAH.");
+        System.out.println("Yearly salary: " + yearlySalary + " UAH.");
+    }
+}
