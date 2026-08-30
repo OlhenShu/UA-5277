8a2d2847ca7fd2bd2e23977d6a247e22c91501ad
ViktorMv <viktormv2002@gmail.com>
2026-08-30 22:16:07 +0200

    Add RandomNumber class for number guessing game

---
+++ src/com/softserve/academy/homework/Topic5/Task4/RandomNumber.java
@@ -0,0 +1,32 @@
+package com.softserve.academy.homework.Topic5.Task4;
+
+import java.util.Scanner;
+
+public class RandomNumber {
+    static Scanner sc = new Scanner(System.in);
+
+    static void main(String[] args) {
+
+        int num;
+        int randomNum = (int) (Math.random() * 100 + 1);
+        int count = 1;
+        do {
+            System.out.println("Enter a number: ");
+            num = sc.nextInt();
+
+            if (num > randomNum) {
+                System.out.println("Too high, try again..");
+                count++;
+
+            } else if (num < randomNum) {
+                System.out.println("Too low, try again..");
+                count++;
+
+            } else {
+                System.out.println("Random number is: " + randomNum);
+                System.out.println("Congratulations! You guessed the number. Number of attempts: " + count);
+            }
+
+        } while (num != randomNum);
+    }
+}
